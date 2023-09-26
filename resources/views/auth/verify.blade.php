37c6185df98ddaaadbb51ad2e0b55bec8d99375a
fqueta <ger.maisaqui1@gmail.com>
2023-09-26 05:33:46 -0300

    26/09/2023 01

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,1 +1,37 @@
-@extends('adminlte::auth.verify')
+{{-- @extends('adminlte::auth.verify') --}}
+@extends('adminlte::auth.auth-page', ['auth_type' => 'login'])
+
+@section('auth_header', __('adminlte::adminlte.verify_message'))
+
+@section('auth_body')
+
+    @if(session('resent'))
+        <div class="alert alert-success" role="alert">
+            {{ __('adminlte::adminlte.verify_email_sent') }}
+        </div>
+    @endif
+    @php
+      $send = session('message-very');
+    @endphp
+    @if ($send=='enviado')
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Sucesso:</strong>
+            <span>link de verificação foi enviado, por favor acesse a caixa de entrada do seu e-mail</span>
+            <span>Caso não encontre procure na caixa de span ou na lixeira.</span>
+         </div>
+
+    @else
+        {{ __('adminlte::adminlte.verify_check_your_email') }}
+        {{ __('adminlte::adminlte.verify_if_not_recieved') }},
+
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <input type="hidden" name="send-email" value="s" />
+            <div class="col-12 text-center">
+                <button type="submit" class="btn btn-primary mt-2 align-baseline">
+                    {{ __('adminlte::adminlte.verify_request_another') }}
+                </button>.
+            </div>
+        </form>
+    @endif
+@stop
