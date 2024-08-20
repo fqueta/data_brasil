d49085da45387b1286cd073a989fed1f3a235fad
fqueta <ger.maisaqui1@gmail.com>
2024-08-20 05:33:19 -0300

    20/08/2024

--- config/app.php
+++ config/app.php
@@ -12,8 +12,8 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '3.0.0',
-    'last_update' => '18/05/2024',
+    'version' => '3.0.1',
+    'last_update' => '20/08/2024',
     'name' => config('adminlte.title'),//env('APP_NAME', 'Data Brasil'),
 
     /*
